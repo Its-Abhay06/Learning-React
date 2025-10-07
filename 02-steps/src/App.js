2b39592172f084e1aca86ae782141d96c6dc21d2
Its-Abhay06 <abhay.mani29@gmail.com>
2025-10-08 01:18:11 +0530

    completed steps

--- 02-steps/src/App.js
+++ 02-steps/src/App.js
@@ -1,7 +1,64 @@
-import React from "react";
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
 
 const App = () => {
-  return <div className="steps">we</div>;
+  const [step, setStep] = useState(1);
+  // const [name] = useState({ name: "Abhay" }); manually changing state by destructuring object not a good practice
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handlePrevious() {
+    setStep(step > 1 ? step - 1 : step);
+  }
+
+  function handleNext() {
+    setStep(step < 3 ? step + 1 : step);
+    // name.name = "tripathi"; manually changing state
+  }
+
+  function handleClose() {
+    setIsOpen(!isOpen);
+  }
+
+  return (
+    <>
+      <button className="close" onClick={handleClose}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+            <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+            <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+          </div>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+            {/* {name.name} manually changing state */}
+          </p>
+
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default App;
