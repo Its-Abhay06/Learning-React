dfcd65f856f161f3cc98db4d69d5861985437e32
Its-Abhay06 <abhay.mani29@gmail.com>
2025-10-09 02:19:10 +0530

    started with travel-list project

---
+++ 03-travel-list/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Logo from "./Components/Logo";
+import Form from "./Components/ Form";
+import PackingList from "./Components/PackingList";
+import Stats from "./Components/Stats";
+
+const App = () => {
+  return (
+    <div>
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+};
+
+export default App;
